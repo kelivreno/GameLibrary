a3323ff2add2c41c5fbba8fdd4e5dbcf5853b1b4
Kelangit Hakim <90366335+kelivreno@users.noreply.github.com>
2026-09-21 21:08:06 -0700

    added a Game struct and use its fields

--- main.go
+++ main.go
@@ -2,19 +2,43 @@ package main
 
 import "fmt"
 
-func main() {
-	gameName := "Minecraft"
-	rating := 9
-	finished := false
-	hoursPlayed := 125
-	developer := "Mojang"
-	price := 29.99
+type Game struct {
+	Name string
+	Rating int
+	Finished bool
+	HoursPlayed int
+	Developer string
+	Price float64
+}
 
-	fmt.Println(gameName)
-	fmt.Println(rating)
-	fmt.Println(finished)
-	fmt.Println(hoursPlayed)
-	fmt.Println(developer)
-	fmt.Println(price)
+func main() {
+	minecraft := Game {
+		Name: "Minecraft",
+		Rating: 9,
+		Finished: false,
+		HoursPlayed: 125,
+		Developer: "Mojang",
+		Price: 29.99,
+
+	}
+
+	fmt.Println(minecraft.Rating)
+	fmt.Println(minecraft.Finished)
+
+	minecraft.Rating = 10
+	minecraft.Finished = true
+
+	fmt.Println(minecraft.Rating)
+	fmt.Println(minecraft.Finished)
+
+
+
+
+	// fmt.Println(gameName)
+	// fmt.Println(rating)
+	// fmt.Println(finished)
+	// fmt.Println(hoursPlayed)
+	// fmt.Println(developer)
+	// fmt.Println(price)
 	
 }
