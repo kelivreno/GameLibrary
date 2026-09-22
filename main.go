945011d5b236e0a5ea7a6081712ef669579f1bd5
Kelangit Hakim <90366335+kelivreno@users.noreply.github.com>
2026-09-21 20:37:00 -0700

    adding more variables and types

--- main.go
+++ main.go
@@ -6,8 +6,15 @@ func main() {
 	gameName := "Minecraft"
 	rating := 9
 	finished := false
+	hoursPlayed := 125
+	developer := "Mojang"
+	price := 29.99
 
 	fmt.Println(gameName)
 	fmt.Println(rating)
 	fmt.Println(finished)
+	fmt.Println(hoursPlayed)
+	fmt.Println(developer)
+	fmt.Println(price)
+	
 }
