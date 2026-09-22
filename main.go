a885a8efce4e655f5a40afe209d15269b92e98e9
Kelangit Hakim <90366335+kelivreno@users.noreply.github.com>
2026-09-21 21:17:37 -0700

    add multiple Game values

--- main.go
+++ main.go
@@ -3,42 +3,44 @@ package main
 import "fmt"
 
 type Game struct {
-	Name string
-	Rating int
-	Finished bool
+	Name        string
+	Rating      int
+	Finished    bool
 	HoursPlayed int
-	Developer string
-	Price float64
+	Developer   string
+	Price       float64
 }
 
 func main() {
-	minecraft := Game {
-		Name: "Minecraft",
-		Rating: 9,
-		Finished: false,
+	minecraft := Game{
+		Name:        "Minecraft",
+		Rating:      9,
+		Finished:    false,
 		HoursPlayed: 125,
-		Developer: "Mojang",
-		Price: 29.99,
-
+		Developer:   "Mojang",
+		Price:       29.99,
 	}
 
-	fmt.Println(minecraft.Rating)
-	fmt.Println(minecraft.Finished)
+	terraria := Game{
+		Name:        "Terraria",
+		Rating:      8,
+		Finished:    true,
+		HoursPlayed: 80,
+		Developer:   "Re-logic",
+		Price:       9.99,
+	}
 
-	minecraft.Rating = 10
-	minecraft.Finished = true
+	stardewValley := Game{
+		Name:        "Stardew Valley",
+		Rating:      10,
+		Finished:    false,
+		HoursPlayed: 50,
+		Developer:   "ConcernedApe",
+		Price:       14.99,
+	}
 
-	fmt.Println(minecraft.Rating)
-	fmt.Println(minecraft.Finished)
+	fmt.Println(minecraft.Name)
+	fmt.Println(terraria.Name)
+	fmt.Println(stardewValley.Name)
 
-
-
-
-	// fmt.Println(gameName)
-	// fmt.Println(rating)
-	// fmt.Println(finished)
-	// fmt.Println(hoursPlayed)
-	// fmt.Println(developer)
-	// fmt.Println(price)
-	
 }
